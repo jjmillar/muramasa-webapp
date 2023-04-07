46f7a0df03af3c638417c468c1a8a981b1dabc7a
Julián Millar <julianjmillar@gmail.com>
2023-04-07 09:54:58 -0400

    added mobile responsive

--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -1,39 +1,20 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import InvertedLogo from "../assets/invertedLogo";
-import OriginalLogo from "../assets/originalLogo";
 import Horarios from "../components/horarios";
 import Costos from "../components/costos";
 import Contacto from "../components/contacto";
+import Text from "../components/text";
 
 import classes from "./home.module.scss";
 
-const arrayOfImg = [<InvertedLogo />, <OriginalLogo />];
-
 const Home = () => {
-  const [imgCounter, setImgCounter] = useState(0);
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setImgCounter((currentValue) => {
-        if (currentValue + 1 === arrayOfImg.length) {
-          return 0;
-        }
-        return currentValue + 1;
-      });
-    }, 3000);
-
-    return () => clearInterval(interval);
-  }, []);
-
   return (
     <div className={classes.wrapper}>
       <div className={classes.description}>
         <div className={classes.text}>
-          "El verdadero propósito de las artes marciales es la lucha contra uno
-          mismo, no contra los demás."
+          <Text />
         </div>
         <div className={classes.efecto}>MURAMASA GRAPPLING ARTS</div>
-
         <div className={classes.infoCelWrapper}>
           <div className={classes.infoCel}>
             Horarios
@@ -55,7 +36,9 @@ const Home = () => {
           </div>
         </div>
       </div>
-      <div className={classes.logo}>{arrayOfImg[0]}</div>
+      <div className={classes.logo}>
+        <InvertedLogo />
+      </div>
     </div>
   );
 };
