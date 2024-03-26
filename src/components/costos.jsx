8a2a1ce24487226536b604d4dd413d47684efdf3
Julián Millar <julianjmillar@gmail.com>
2024-03-26 15:37:16 -0300

    chore: updated info

--- src/components/costos.jsx
+++ src/components/costos.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Costos = () => {
   return (
     <div>
-      <p>- $25.000.- por asistir a todas las clases. Por mes.</p>
-      <p>- $20.000.- una o dos veces a la semana. Por mes.</p>
-      <p>- $4.000.- cada clase individual.</p>
+      <p>- $30.000.- por asistir a todas las clases. Por mes.</p>
+      <p>- $25.000.- una o dos veces a la semana. Por mes.</p>
+      <p>- $5.000.- cada clase individual.</p>
     </div>
   );
 };
--- src/components/horarios.jsx
+++ src/components/horarios.jsx
@@ -3,10 +3,10 @@ import React from "react";
 const Horarios = () => {
   return (
     <div>
-      <p>- Martes: 21:00-23:00 / Gi / Clase de Fundamentales</p>
-      <p>- Jueves: 21:00-23:00 / NoGi / Clase de BJJ y Grappling</p>
-      <p>- Viernes: 21:00-23:00 / Gi / Clase avanzada</p>
-      <p>- Dirección: Camilo Henriquez #510, Los Ángeles</p>
+      <p>- Lunes: 20:00-22:00 / Gi / Clase de Fundamentales</p>
+      <p>- Miércoles: 21:00-23:00 / NoGi / Clase de BJJ y Grappling</p>
+      <p>- Viernes: 21:00-23:00 / NoGi / Clase avanzada</p>
+      <p>- Dirección: José Manso de Velasco #474, Los Ángeles</p>
     </div>
   );
 };
