6c306d7e54b9bffbcd792ee786ab676ce0e66e1e
Julián Millar <julianjmillar@gmail.com>
2024-05-29 11:46:27 -0400

    docs: modified horarios

--- src/components/horarios.jsx
+++ src/components/horarios.jsx
@@ -4,8 +4,8 @@ const Horarios = () => {
   return (
     <div>
       <p>- Lunes: 20:00-22:00 / Gi / Clase de Fundamentales</p>
-      <p>- Miércoles: 21:00-23:00 / NoGi / Clase de BJJ y Grappling</p>
-      <p>- Viernes: 21:00-23:00 / NoGi / Clase avanzada</p>
+      <p>- Miércoles: 20:00-22:00 / NoGi / Clase de BJJ y Grappling</p>
+      <p>- Viernes: 20:00-22:00 / NoGi / Clase de BJJ y Grappling</p>
       <p>- Dirección: José Manso de Velasco #474, Los Ángeles</p>
     </div>
   );
