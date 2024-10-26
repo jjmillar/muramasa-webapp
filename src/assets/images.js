6295427545c93452ee77ff81bcc8b7124b968dfc
Julián Millar <julianjmillar@gmail.com>
2024-10-26 09:01:43 -0300

    muramasa web 2.0

--- src/assets/images.js
+++ src/assets/images.js
@@ -1,27 +1,27 @@
 const images = [
   {
     id: '1',
-    image: '1.jpg'
+    image: '1.webp'
   },
   {
     id: '2',
-    image: '2.jpg'
+    image: '2.webp'
   },
   {
     id: '3',
-    image: '3.jpg'
+    image: '3.webp'
   },
   {
     id: '4',
-    image: '4.jpg'
+    image: '4.webp'
   },
   {
     id: '5',
-    image: '5.jpg'
+    image: '5.webp'
   },
   {
     id: '6',
-    image: '6.jpg'
+    image: '6.webp'
   }
 ]
 
