78f62853d8cd3813f5cdba4885b4d2c20a82f54e
Julián Millar <julianjmillar@gmail.com>
2024-10-26 09:14:30 -0300

    refactor: added aria-label to navbar buttons

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,10 +3,10 @@ import { call, whatsapp, openCalendar, openInsta } from '../assets/utils'
 const Navbar = () => {
   return (
     <nav id='navbar' className='navbar' key='navbar'>
-      <button type='button' className='navbar__btn' onClick={call}><ion-icon name='call-outline' /></button>
-      <button type='button' className='navbar__btn' onClick={whatsapp}><ion-icon name='logo-whatsapp' /></button>
-      <button type='button' className='navbar__btn' onClick={openCalendar}><ion-icon name='calendar-number-outline' /></button>
-      <button type='button' className='navbar__btn' onClick={openInsta}><ion-icon name='logo-instagram' /></button>
+      <button type='button' className='navbar__btn' onClick={call}><ion-icon name='call-outline' aria-label='call' /></button>
+      <button type='button' className='navbar__btn' onClick={whatsapp}><ion-icon name='logo-whatsapp' aria-label='whatsapp' /></button>
+      <button type='button' className='navbar__btn' onClick={openCalendar}><ion-icon name='calendar-number-outline' aria-label='calendar' /></button>
+      <button type='button' className='navbar__btn' onClick={openInsta}><ion-icon name='logo-instagram' aria-label='instagram' /></button>
     </nav>
   )
 }
